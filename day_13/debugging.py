46c64cfed332eb334c575e36f33077dc2e258770
ugurfatihcokuckun <ugurfatihcokuckun@gmail.com>
2023-07-03 20:11:57 +0300

    debugging

---
+++ day_13/debugging.py
@@ -0,0 +1,44 @@
+############DEBUGGING#####################
+
+# # Describe Problem
+# def my_function():
+#   for i in range(1, 20):        #20 => 21
+#     if i == 20:
+#       print("You got it")
+# my_function()
+
+# # Reproduce the Bug
+# from random import randint
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(1, 6)          #1 => 0 / 6 => 5 
+# print(dice_imgs[dice_num])
+
+# # Play Computer
+# year = int(input("What's your year of birth?"))
+# if year > 1980 and year < 1994:
+#   print("You are a millenial.")
+# elif year > 1994:       # > => >=
+#   print("You are a Gen Z.")
+
+# # Fix the Errors
+# age = input("How old are you?")     #int()
+# if age > 18:
+#     print("You can drive at age {age}.")     #f string 
+
+# #Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: "))      # == => =
+# total_words = pages * word_per_page
+# print(total_words)
+
+# #Use a Debugger
+# def mutate(a_list):
+#   b_list = []
+#   for item in a_list:
+#     new_item = item * 2
+#   b_list.append(new_item)    #indentation 1 right
+#   print(b_list)
+
+# mutate([1,2,3,5,8,13])
