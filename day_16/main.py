8edd8d2c6174a0a9a0df67b969a4824486a3bd83
ugurfatihcokuckun <ugurfatihcokuckun@gmail.com>
2023-07-04 17:32:14 +0300

    turtle

---
+++ day_16/main.py
@@ -0,0 +1,10 @@
+import turtle
+
+timmy = turtle.Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.forward(100)
+timmy.color("red", "green")
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
